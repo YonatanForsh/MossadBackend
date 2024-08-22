9bb9fd7101488ac04dc8beb734c5a6b6d551a870
Yonatan <yonatanforsh@gmail.com>
2024-08-22 09:33:51 +0300

    Dictionary of directions to move agents and targets

--- Directions/Direction.cs
+++ Directions/Direction.cs
@@ -2,18 +2,16 @@
 {
     public class Direction
     {
-        Dictionary<string, int> _direction = new Dictionary<string, int>()
+        Dictionary<string, List<int>> _direction = new Dictionary<string, List<int>>()
         {
-            { "nw", -1 },
-            { "n" , 1 },
-            { "ne" , 1 },
-            { "w" , 1 },
-            { "e" , 1 },
-            { "sw" , 1 },
-            { "s" , 1 },
-            { "se" , 1 },
+            { "nw", [-1, 1] },
+            { "n" , [0, 1] },
+            { "ne" , [1, 1] },
+            { "w" , [-1, 0] },
+            { "e" , [1,0] },
+            { "sw" , [-1, -1] },
+            { "s" , [0, -1] },
+            { "se" , [1, -1] }
         };
     }
-
-    
 }
