6255436be0a79a4fc500a4f2abc4585948b6b3b4
Yonatan <yonatanforsh@gmail.com>
2024-08-22 09:13:33 +0300

    Created dictionary of direction

---
+++ Directions/Direction.cs
@@ -0,0 +1,19 @@
+﻿namespace MossadBackend.Directions
+{
+    public class Direction
+    {
+        Dictionary<string, int> _direction = new Dictionary<string, int>()
+        {
+            { "nw", -1 },
+            { "n" , 1 },
+            { "ne" , 1 },
+            { "w" , 1 },
+            { "e" , 1 },
+            { "sw" , 1 },
+            { "s" , 1 },
+            { "se" , 1 },
+        };
+    }
+
+    
+}
